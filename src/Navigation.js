4b65e1da67490de074dc32f2ed121d5cefe0e0f2
Luong Chi Cuong <luongchicuong93@gmail.com>
2017-07-08 23:38:30 +0700

    add folder

---
+++ src/Navigation.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import {Icon} from 'react-materialize';
+// import logo from './logo.svg';
+import './Navigation.css';
+
+class Navigation extends Component {
+  // constructor(props) {
+  //   super(props);
+  // };
+  render() {
+    const items = ["Overal", "Bookmarks", "Category", "E-books", "Accounts"];
+    const listMenu = items.map((item, index) =>
+      <li key={index}>
+        <Icon tiny>bookmark</Icon>{item}
+      </li>
+    );
+    return (
+      <div className="App-navigation">
+        <h2 className="App-logo">DashBoard</h2>
+        <div className="App-menu">
+          <ul>
+            {listMenu}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Navigation;
