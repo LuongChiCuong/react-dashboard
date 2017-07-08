ed090f2eb843dfbf1d3e8624a3adb5bbd3c00fe5
LuongChiCuong <luongchicuong93@gmail.com>
2017-07-08 23:41:16 +0700

    first push source-tree

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Navigation from './Navigation'
+// import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  // constructor(props) {
+  //   super(props);
+  // };
+  render() {
+    return (
+      <div className="App">
+        <Navigation></Navigation>
+      </div>
+    );
+  }
+}
+
+export default App;
